f64c287f799595011b89cb5f8498d8da8b735106
Gustavo Leimbek <143670858+gleimbek@users.noreply.github.com>
2026-05-08 07:14:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// Scroll simple animation (reveal effect)
 
+const sections = document.querySelectorAll(".section");
+
+window.addEventListener("scroll", () => {
+  sections.forEach(sec => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 500;
+
+    if (top > offset) {
+      sec.style.opacity = "1";
+      sec.style.transform = "translateY(0)";
+      sec.style.transition = "0.6s ease";
+    }
+  });
+});
